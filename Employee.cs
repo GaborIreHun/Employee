f142d2b1501f30b711ae4674ca3e08ab3575ce39
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-14 19:42:04 +0000

    Add files via upload
    C# 6 for Programmers, Sixth Edition by Paul Deitel & Harvey Deitel; Excercise 4.11 - 5 weeks into leaning C#

---
+++ Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Employee
+{
+    public string firstName;
+
+    public string lastName;
+
+    public decimal monthlySalary;
+
+    
+
+    
+
+
+    
+    public void SetFirstName(string employeeFirstName)
+    {
+        firstName = employeeFirstName;
+    }
+
+    public string GetFirstName()
+    {
+        return firstName;
+    }
+
+    public void SetLastName(string employeeLastName)
+    {
+        lastName = employeeLastName;
+    }
+
+    public string GetLastName()
+    {
+        return lastName;
+    }
+
+    public void SetMonthlySalary(decimal employeeMonthlySalary)
+    {
+        monthlySalary = employeeMonthlySalary;
+        while (monthlySalary < 0)
+        {
+            Console.WriteLine("Monthly salary should be positive number!");
+            break;
+        }
+        
+               
+    }
+
+    public decimal GetMonthlySalary()
+    {
+        return monthlySalary;
+    }
+
+    
+
+}
---
+++ EmployeeTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class EmployeeTest
+{
+    static void Main()
+    {
+        
+
+
+
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+
+
+        
+        Console.Write($"Please enter the first employee's first name: ");
+        string theFirstName = Console.ReadLine();
+        employee1.SetFirstName(theFirstName);
+
+        Console.Write($"Please enter the first employee's last name: ");
+        string theLastName = Console.ReadLine();
+        employee1.SetLastName(theLastName);
+
+        Console.Write($"Please enter the first employee's mothly salary: ");
+        decimal theMonthlySalary = decimal.Parse(Console.ReadLine());
+        employee1.SetMonthlySalary(theMonthlySalary);
+
+        Console.Write($"Please enter the second employee's first name: ");
+        string theSecondFirstName = Console.ReadLine();
+        employee2.SetFirstName(theSecondFirstName);
+
+        Console.Write($"Please enter the second employee's last name: ");
+        string theSecondLastName = Console.ReadLine();
+        employee2.SetLastName(theLastName);
+
+        Console.Write($"Please enter the second employee's mothly salary: ");
+        decimal theSecondMonthlySalary = decimal.Parse(Console.ReadLine());
+        employee2.SetMonthlySalary(theSecondMonthlySalary);
+
+
+        Console.WriteLine($"\nThe first employee's name: {employee1.GetFirstName()} {employee1.GetLastName()}; his/her mothly salary is: {employee1.GetMonthlySalary()}.");
+        Console.WriteLine($"The annual salary of {employee1.GetFirstName()}'s is {employee1.GetMonthlySalary() * 12}.");
+        Console.WriteLine($"{employee1.GetFirstName()}'s annual salary for next year(after 10% increment) is {(employee1.GetMonthlySalary() * 12) + ((employee1.GetMonthlySalary() * 12) / 10)}");
+
+
+        Console.WriteLine($"\nThe second employee's name: {employee2.GetFirstName()} {employee2.GetLastName()}; his/her mothly salary is {employee2.GetMonthlySalary()}.");
+        Console.WriteLine($"The annual salary of {employee2.GetFirstName()}'s is {employee2.GetMonthlySalary() * 12}.");
+        Console.WriteLine($"{employee2.GetFirstName()}'s annual salary for next year(after 10% increment) is {(employee2.GetMonthlySalary() * 12) + ((employee2.GetMonthlySalary() * 12) / 10)}");
+
+    }
+}
+  
